6533d334b6b2a96009cc06554ff796170c1a2b9a
MAlexa <aryalexa@gmail.com>
2025-05-01 20:47:28 +0200

    mod9 - upd
    - class template used
    - time comparison added

--- mod09/ex02/PmergeMe.hpp
+++ mod09/ex02/PmergeMe.hpp
@@ -9,15 +9,17 @@
 #include <vector>
 #include <algorithm>
 
-static size_t min(size_t n1, size_t n2);
-static unsigned int index_t(unsigned int k);
+size_t min(size_t n1, size_t n2);
+unsigned int index_t(unsigned int k);
+
+// ------------------------------------------------
 
 template <typename C>
 void print_nums(const std::string &msg, const C &nums) {
 	std::cout << msg << "\t";
-	typename C::const_iterator it;
-	for (it = nums.begin(); it!=nums.end(); ++it) {
-		std::cout << (*it) << " ";
+	typename C::const_iterator it1;
+	for (it1 = nums.begin(); it1!=nums.end(); ++it1) {
+		std::cout << (*it1) << " ";
 	}
 	std::cout << std::endl;
 }
@@ -38,23 +40,133 @@ std::ostream& operator<<(std::ostream& out, const std::vector<T>& set)
     return out << " ]";
 }
 
+template <typename C1, typename C2>
+void assert_same(const C1 &c1, const C2 &c2) {
+	if (c1.size() != c2.size())
+		throw std::runtime_error("size is not the same!");
+	typename C1::const_iterator it1 = c1.begin();
+	typename C2::const_iterator it2 = c2.begin();
+	while (it1 != c1.end() && it2 != c2.end()) {
+		if ( *it1 != *it2)
+			throw std::runtime_error("contain is not the same");
+		++it1;
+		++it2;
+	}
+}
 
-class PmergeMe1
+// ------------------------------------------------
+
+template <typename contType>
+class PmergeMe
 {
 private:
-	typedef std::vector<int> contType;
+	//typedef std::vector<int> contType;
 	
-	PmergeMe1();
-	PmergeMe1(const PmergeMe1 &other);
-	PmergeMe1 &operator=(const PmergeMe1 &other);
-	~PmergeMe1();
+	PmergeMe();
+	PmergeMe(const PmergeMe &other);
+	PmergeMe &operator=(const PmergeMe &other);
+	~PmergeMe();
 	
 	static void mergeinsert_sort(contType &c);
 	static void binary_insertion(contType &c, int elem, size_t limit);
 	static int binarySearch(const contType &a, int elem, int low, int high);
 	
 public:
-	static const std::string CONTAINER_NAME;
-	static void sort(std::list<int> nums);
+	static contType sort(std::list<int> nums);
 
 };
+
+// ------------------------------------------------
+
+/**
+ * returns position to insert
+ * - low and high are both included in the search.
+ */
+template <typename contType>
+int PmergeMe<contType>::binarySearch(const contType &a, int elem, int low, int high)
+{
+	// std::cout << "   binSearch " << elem 
+	// 	<< " in " << a << " [" << low << "," << high << "]" << std::endl;
+    if (high <= low)
+        return (elem > a[low]) ? (low + 1) : low;
+    int mid = (low + high) / 2;
+    // if (elem == a[mid])
+    //     return mid + 1;
+    if (elem > a[mid])
+        return binarySearch(a, elem, mid + 1, high);
+    return binarySearch(a, elem, low, mid - 1);
+}
+
+template <typename contType>
+void PmergeMe<contType>::binary_insertion(contType &c, int elem, size_t limit) {
+	
+	size_t loc = binarySearch(c, elem, 0, limit);
+	//std::cout << "  insert " << elem << " in loc " << loc << " of " << c << std::endl;;
+	c.insert(c.begin() + loc, elem);
+}
+
+template <typename contType>
+void PmergeMe<contType>::mergeinsert_sort(contType &d) {
+	size_t n = d.size();
+	if (n == 1) return;
+
+	// 1. pairwise comparison
+	contType a; //big
+	contType b; //small
+	for (size_t i = 0; i < n/2; ++i) {
+		if (d[i*2] > d[i*2 + 1]) {
+			a.push_back(d[i*2]);
+			b.push_back(d[i*2 + 1]);
+		} else {
+			a.push_back(d[i*2 + 1]);
+			b.push_back(d[i*2]);	
+		}
+	}
+	if (n % 2 == 1)
+		b.push_back(d[n - 1]);
+
+	// 2. Recursion and renaming
+	std::map<int, int> m;
+	for (size_t i = 0; i < n/2; ++i) {
+		m[a[i]] = b[i];
+	}
+	mergeinsert_sort(a);
+	for (size_t i = 0; i < n/2; ++i) {
+		b[i] = m[a[i]];
+	}
+
+	// 3. Insertion
+	d = a;
+	d.insert(d.begin(), b[0]);
+	// t_k in [1..n]. The indices are adjusted to [0..n-1] when used
+	unsigned int k = 2; 
+	unsigned int upper_half_n = n/2 + n%2;
+	while (index_t(k - 1) < upper_half_n) {
+		// from: first element of the batch
+		size_t from = ::min(index_t(k), upper_half_n) - 1; //idx0
+		// u: position of a[from] in d
+		size_t u = index_t(k - 1) + from; 
+
+		// k-th batch: 
+		// the elements b[t_k, t_k-1, .., t_(k-1)+1]
+		// are inserted in that order
+		for (size_t i = from; i >= index_t(k - 1) + 1 - 1; --i) { //idx0
+			binary_insertion(d, b[i], u - 1);
+			// adjust u: position of a[i] in d
+			while (d[u] != a[i - 1])
+				--u;
+		}
+		++k;
+	}
+}
+
+template <typename contType>
+contType PmergeMe<contType>::sort(std::list<int> nums) {
+	// fill cont
+	contType c = contType(nums.begin(), nums.end());
+	// sort
+	mergeinsert_sort(c);
+	return c;
+}
+
+// ------------------------------------------------
--- mod09/ex02/main.cpp
+++ mod09/ex02/main.cpp
@@ -4,7 +4,12 @@
 #include <sstream>
 #include <list>
 #include <set>
+#include <vector>
+#include <deque>
 #include "PmergeMe.hpp"
+#include <sys/time.h>
+#include <iomanip>
+
 
 
 bool  in_set(std::set<int> s, int elem) {
@@ -48,11 +53,41 @@ int main(int argn, char *argv[]) {
 		std::list<int> nums = get_args_as_ints(argn, argv);
 		if (nums.size() < 1)
 			return 0;
-		print_nums("input", nums);
+		print_nums("Before:", nums);
+
+		timeval start, end;
 
 		//c1
-		PmergeMe1::sort(nums);
+		const std::string container1 = "std::deque<int>";
+		std::deque<int> res1;
+		gettimeofday(&start, NULL);
+		res1 = PmergeMe< std::deque<int> >::sort(nums);
+		gettimeofday(&end, NULL);
+		double time1 = (end.tv_sec - start.tv_sec) * 1000.0;      // sec to ms
+		time1 += (end.tv_usec - start.tv_usec) / 1000.0;          // us to ms
+
 		//c2
+		const std::string container2 = "std::vector<int>";
+		std::vector<int> res2;
+		gettimeofday(&start, NULL);
+		res2 = PmergeMe< std::vector<int> >::sort(nums);
+		gettimeofday(&end, NULL);
+		double time2 = (end.tv_sec - start.tv_sec) * 1000.0;      // sec to ms
+		time2 += (end.tv_usec - start.tv_usec) / 1000.0;          // us to ms
+
+		assert_same(res1, res2);
+		print_nums("After:", res1);
+		//print_nums("result", res2);
+		/**
+		 * Time to process a range of 5 elements with std::[..] : 0.00031 us
+		 * Time to process a range of 5 elements with std::[..] : 0.00014 us
+		 */
+		std::cout << "Time to process a range of " << nums.size() << " elements with " <<
+			std::setw(16) << 
+			container1 << " : " << time1 << " ms" << std::endl;
+		std::cout << "Time to process a range of " << nums.size() << " elements with " <<
+			std::setw(16) << 
+			container2 << " : " << time2 << " ms" << std::endl;
 
 	} catch (std::exception &e) {
 		std::cout << "Error: " << e.what() << std::endl;
